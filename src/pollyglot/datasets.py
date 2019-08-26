f209f2c14de5dae9f3aa95bb2edfad034d150791
NickleDave <nickledave@users.noreply.github.com>
2019-08-25 22:41:55 -0400

    add dataset.py module to pollyglot/

---
+++ src/pollyglot/datasets.py
@@ -0,0 +1,16 @@
+import json
+from pathlib import Path
+
+import yaml
+
+
+HERE = Path(__file__).parent
+DATASETS_YAML = HERE.joinpath('datasets.yaml')
+with DATASETS_YAML.open() as f:
+    dataset_dict = yaml.safe_load(f)
+
+dataset_names = [k for k in dataset_dict.keys()]
+
+
+def show():
+    print(json.dumps(dataset_dict, indent=1))
