9adeb685490a410380d9af1a096d5144447373f4
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-30 11:22:17 +0000

    Adding tarfile member sanitization to extractall()

--- src/pollyglot/fetchfuncs.py
+++ src/pollyglot/fetchfuncs.py
@@ -447,7 +447,26 @@ def fetch(dataset_name, dst='.', remove_compressed_file=True):
 
         if file_name.endswith('.tar.gz'):
             with tarfile.open(file_name) as tar:
-                tar.extractall(path=dst)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=dst)
 
         elif file_name.endswith('.zip'):
             with ZipFile(file_name, 'r') as zipfile:
